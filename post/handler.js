584b73325355a07924e30a8d85b897074acc3b17
hewell <hewell@gmail.com>
2016-06-29 03:16:47 +0800

    add dynamodb dependency

--- post/handler.js
+++ post/handler.js
@@ -1,7 +1,13 @@
 'use strict';
 
+var doc = require('dynamodb-doc');
+var dynamo = new doc.DynamoDB();
+
 module.exports.handler = function(event, context, cb) {
-  return cb(null, {
+    
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    
+    return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
+    });
 };
