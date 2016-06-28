53d27cccc4d088b48000b0aec9909a0df6e37274
hewell <hewell@gmail.com>
2016-06-29 03:19:41 +0800

    define operation "create"

--- post/handler.js
+++ post/handler.js
@@ -7,6 +7,17 @@ module.exports.handler = function(event, context, cb) {
     
     console.log('Received event:', JSON.stringify(event, null, 2));
     
+    var operation = event.operation;
+    if(event.tableName){
+        event.payload.TableName = event.tableName;
+    }
+    
+    switch(operation){
+        case "create":
+            dynamo.putItem(event.payload, context.done);
+            break;
+            
+    }
     return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
     });
