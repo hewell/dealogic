41851c79d9266a4fc54915d9a5cc752d197747e1
hewell <hewell@gmail.com>
2016-07-01 02:56:33 +0800

    read and parse xml feed into json

--- post/handler.js
+++ post/handler.js
@@ -3,19 +3,45 @@
 var doc = require('dynamodb-doc');
 var dynamo = new doc.DynamoDB();
 
-module.exports.handler = function(event, context, cb) {
-    
+module.exports.handler = function (event, context, cb) {
+
     console.log('Received event:', JSON.stringify(event, null, 2));
-    
+
     var operation = event.operation;
-    if(event.tableName){
+    if (event.tableName) {
         event.payload.TableName = event.tableName;
     }
-    
-    switch(operation){
-        case "create":
-            event.payload.Item.id = event.payload.Item.content.id;
-            dynamo.putItem(event.payload, context.done);
-            break;
+
+    switch (operation) {
+    case "create":
+        event.payload.Item.id = event.payload.Item.content.id;
+        saveFeed(event.payload.Item.content.host, event.payload.Item.content.path, event.payload, context);
+        break;
     }
+};
+
+function saveFeed(host, path, payload, context) {
+    var http = require('http'),
+        xml2js = require('xml2js'),
+        parser = new xml2js.Parser(),
+        concat = require('concat-stream');
+
+    console.log('Read from url: ', host + path);
+    return http.get({
+        host: host,
+        path: path
+    }, function (response) {
+        response.pipe(concat(function (body) {
+            parser.parseString(body, function (err, result) {
+                if (err) {
+                    console.log('Got error: ' + err.message);
+                } else {
+                    payload.Item.json = JSON.stringify(result);
+                    dynamo.putItem(payload, context.done);
+                    console.log('Done.');
+                }
+            });
+        }))
+    });
+
 };
